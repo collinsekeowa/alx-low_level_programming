be4dee76a0833fb5f83b7bf45e590c5356a7a3d8
collinsekeowa <collins.ekeowa@gmail.com>
2023-02-16 18:40:36 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print a line to standard error
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
