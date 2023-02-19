c5e0940e88588ab0129e678eae3d7acd832a19bf
collinsekeowa <collins.ekeowa@gmail.com>
2023-02-18 18:13:29 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit num of base 10 from 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d\n", num);
+
+	return (0);
+}
