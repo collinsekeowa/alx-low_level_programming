77e6bba321e0b5c6166173c065bdea7dce12e5b5
collinsekeowa <collins.ekeowa@gmail.com>
2023-02-21 12:14:43 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
 
 void print_alphabet_x10(void)
 {
-	char alpha;
-
 	int count = 0;
+
+	char alpha;
 
 	while (count++ <= 9)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 			_putchar(alpha);
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
