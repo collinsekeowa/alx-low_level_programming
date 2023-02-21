b912348b8557a31508c12029f409d5f933621b5b
collinsekeowa <collins.ekeowa@gmail.com>
2023-02-20 16:00:18 -0800

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Numbers must be separated by ,, followed by a space
+ * The three digits must be different
+ * 012, 120, 102, 021, 201, 210 are considered the same combination
+ * of the three digits 0, 1 and 2
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9', k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i == '7' && j == '8' && k == '9')
+					{
+						break;
+					}
+
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
