12133ddf2d1cb09147a3906595024316eff3aa6c
collinsekeowa <collins.ekeowa@gmail.com>
2023-02-24 13:25:15 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		if (c != 2 || c != 4)
+		if (!(c == 2 || c == 4))
 			_putchar(c);
 	}
 
-i	_putchar('\n');
+	_putchar('\n');
 }
