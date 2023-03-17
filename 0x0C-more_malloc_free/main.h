55c524ce0c5e0321311b1945ee5bcc9afce49156
collinsekeowa <collins.ekeowa@gmail.com>
2023-03-16 18:34:28 -0700

    task 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,6 +12,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #include <stdio.h>
-#include <stdlib.h
+#include <stdlib.h>
 
 #endif
